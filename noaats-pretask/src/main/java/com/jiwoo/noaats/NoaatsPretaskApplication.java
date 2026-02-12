5b99d1840937cd564733931a8945cb6b4bd95de4
yusica09 <choijiwoo0922@gmail.com>
2026-02-13 00:35:24 +0900

    init: spring boot 기본 프로젝트 세팅

---
+++ noaats-pretask/src/main/java/com/jiwoo/noaats/NoaatsPretaskApplication.java
@@ -0,0 +1,13 @@
+package com.jiwoo.noaats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoaatsPretaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoaatsPretaskApplication.class, args);
+	}
+
+}
---
+++ noaats-pretask/src/test/java/com/jiwoo/noaats/NoaatsPretaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiwoo.noaats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoaatsPretaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
